7d9931b5900b48d398d1bc1298791da749c1e7f4
Jerry Wu <wujerry573@gmail.com>
2021-03-28 13:51:04 -0500

    added rest of beans

---
+++ src/com/facility/model/use/InspectionInterface.java
@@ -0,0 +1,63 @@
+package com.facility.model.use;
+
+import com.facility.model.facility.Facility;
+
+public class Inspection {
+
+    private String inspectionId;
+    private Facility facility;
+    private String date;
+    private InspectionType type;
+    private UseSchedule useSchedule;
+
+    public Inspection() {};
+
+    public Inspection(UseSchedule useSchedule) {
+        this.useSchedule = useSchedule;
+    };
+
+    public void setUseSchedule(UseSchedule useSchedule){this.useSchedule = useSchedule;}
+
+    public String getInspectionId() {
+        return inspectionId;
+    }
+
+    public void setInspectionId(String inspectionId) {
+        this.inspectionId = inspectionId;
+    }
+
+    public Facility getFacility() {
+        return facility;
+    }
+
+    public void setFacility(Facility facility) {
+        this.facility = facility;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public InspectionType getType() {
+        return type;
+    }
+
+    public void setType(InspectionType type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Inspection{" +
+                "inspectionId='" + inspectionId + '\'' +
+                ", facility='" + facility.getFacilityId() + '\'' +
+                ", date='" + date + '\'' +
+                ", type='" + type.getInspectionTypeId() + '\'' +
+                ", useSchedule='" + useSchedule.getScheduleId() + '\'' +
+                '}';
+    }
+}
